223ff022e110b302f3929bf758e2cc9863ee34b2
sunyuhong <sunyuhong1030@163.com>
2020-06-09 11:51:13 +0800

    为按钮设置监听器的两种方法

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //使用匿名内部类为按钮设置
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"单击了按钮1",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void myClick(View view){
+        Toast.makeText(MainActivity.this,"单击了按钮2",Toast.LENGTH_SHORT).show();
     }
 }
